353738874727b8f3b0f8058385a7059d1d99b08d
Jonathan Shulgach <45723019+Jshulgach@users.noreply.github.com>
2024-12-15 21:18:24 -0500

    Update 1_save_frames.py camera id

--- 1_save_frames.py
+++ 1_save_frames.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     ap.add_argument("-y", "--yaml_dir", type=str, default='calibration_settings.yaml', help="Path to folder containing calibration settings")
     ap.add_argument("-c", "--camera_id", type=str, default='camera0', help="Camera ID")
     args = vars(ap.parse_args())
-    save_frames_single_camera(args["yaml_dir"], args["img_dir"], args["id"])
+    save_frames_single_camera(args["yaml_dir"], args["img_dir"], args["camera_id"])
